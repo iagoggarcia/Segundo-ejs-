27e171829d7d617db2f8adc7fb24f86e20d01bc4
Iago García <iagogarciaquintela@gmail.com>
2025-09-30 16:22:48 +0200

    More files

---
+++ 1er_cuatri/AED/GarciaQuintelaIago_3/main.c
@@ -0,0 +1,28 @@
+int main (int argc, char *argv[]) {
+    char opcion;
+
+    do {
+        printf("--------MENÚ--------\n");
+        printf("a) Añadir personaje\n");
+        printf("l) Listado alfabético de personajes\n");
+        printf("e) Eliminar personaje\n");        
+        printf("s) Salir\n");
+        printf("--------------------\n");        
+        switch (opcion) {
+        case 'A': case 'a':
+            
+            break;
+        case 'L': case 'l':
+            
+            break;
+        case 'E': case 'e':
+            
+            break;
+        case 'S': case 's':
+            
+            break;    
+        default:
+            break;
+        }
+    } while (opcion != 's' && opcion != 'S');
+}
